ed038d8b48976a01f3b68d9044bc8a361cbc6b22
Tin Tvrtković <tinchester@gmail.com>
2022-03-07 12:57:57 +0100

    Implement validation for estate shapes

--- tests/land/conftest.py
+++ tests/land/conftest.py
@@ -15,5 +15,5 @@ def estate_contract(land_contract: ProjectContract, admin: LocalAccount):
     estates.initialize(
         ESTATE_NAME, ESTATE_SYMBOL, ESTATE_BASE_TOKEN_URI, land_contract.address
     )
-    land_contract.grantRole(land_contract.ESTATE_MANAGER_ROLE(), estates)
+    land_contract.grantRole(land_contract.ESTATE_MANAGER_ROLE(), estates, {"from": admin})
     return estates
--- tests/land/test_estate.py
+++ tests/land/test_estate.py
@@ -15,14 +15,14 @@ def test_estate_minting(
 
     token_ids = [
         0,
-        16777216,
-        33554432,
-        256,
-        16777472,
-        33554688,
-        512,
-        16777728,
-        33554944,
+        65536,
+        131072,
+        1,
+        65537,
+        131073,
+        2,
+        65538,
+        131074
     ]
     for i in token_ids:
         land_contract.mint(alice, i, {"from": admin}).wait(1)
@@ -32,20 +32,19 @@ def test_estate_minting(
 
     # Charlie cannot mint the estate.
     with pytest.raises(VirtualMachineError):
-        tx = estate_contract.mintFromParcels(token_ids, {"from": charlie}).wait(1)
+        estate_contract.mintFromParcels(token_ids, {"from": charlie}).wait(1)
 
-    tx = estate_contract.mintFromParcels(token_ids, {"from": alice})
-    tx.wait(1)
+    (tx := estate_contract.mintFromParcels(token_ids, {"from": alice})).wait(1)
 
     estate_token_id = tx.return_value
 
-    # Alice has no land again.
+    # # Alice has no land again.
     assert land_contract.ownerTokens(alice) == ()
 
-    # Alice has an estate now.
+    # # Alice has an estate now.
     assert estate_contract.balanceOf(alice) == 1
 
-    # Alice cannot use her parcel any more.
+    # # Alice cannot use her parcel any more.
     with pytest.raises(VirtualMachineError):
         land_contract.safeTransferFrom(alice, admin, 0, {"from": alice}).wait(1)
 
@@ -61,3 +60,35 @@ def test_estate_minting(
     # Alice has her tokens again.
     tokens = land_contract.ownerTokens(alice)
     assert set(tokens) == set(token_ids)
+
+
+def test_wrong_shape(
+    land_contract: ProjectContract,
+    estate_contract: ProjectContract,
+    admin: str,
+    alice: str,
+    charlie: str,
+):
+    # Alice has nothing.
+    assert land_contract.ownerTokens(alice) == ()
+
+    token_ids = [
+        0,
+        65536,
+        131072,
+        1,
+        65537,
+        131073,
+        2,
+        65538,
+        131074
+    ]
+    for i in token_ids:
+        land_contract.mint(alice, i, {"from": admin}).wait(1)
+
+    # Alice has 9 tokens.
+    assert set(land_contract.ownerTokens(alice)) == set(token_ids)
+
+    # Alice cannot mint the estate only 8 parcels.
+    with pytest.raises(VirtualMachineError):
+        estate_contract.mintFromParcels(token_ids[:8], {"from": alice}).wait(1)
