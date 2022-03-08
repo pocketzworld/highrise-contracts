1982371e56ad3245cd513e61587e1b71171513cc
Tin Tvrtković <tinchester@gmail.com>
2022-03-08 01:17:53 +0100

    Fixes

--- interfaces/IHighriseLand.sol
+++ interfaces/IHighriseLand.sol
@@ -4,5 +4,6 @@ pragma solidity ^0.8.4;
 
 interface IHighriseLand {
     function mint(address user, uint256 tokenId) external;
-    function bindToEstate(address owner, uint256[] memory tokenIds) external;
+
+    function bindToEstate(address owner, uint32[] calldata tokenIds) external;
 }
