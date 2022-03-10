04b97bfd4d1ff534c1a1ba5947040736d6e82e6f
Marko Culinovic <marko.culinovic@gmail.com>
2022-03-10 09:55:06 +0100

    Scripts cleanup

--- scripts/estate.py
+++ scripts/estate.py
@@ -1,30 +1,29 @@
+from typing import Optional
+
 from brownie import Contract, HighriseEstate, HighriseLand, TransparentUpgradeableProxy
+from eth_account import Account
 
 from . import ESTATE_BASE_URI_TEMPLATE, ESTATE_NAME, ESTATE_SYMBOL
 from .common import encode_function_data, get_account
 
 
-def deploy_estate(
-    land_address: str,
-    environment: str = "dev",
-) -> tuple[Contract, Contract]:
-    account = get_account()
+def deploy_estate_implementation(account: Optional[Account] = None) -> Contract:
+    if not account:
+        account = get_account()
     estate = HighriseEstate.deploy(
         {"from": account},
     )
-    # Do not leave an implementation contract uninitialized. An uninitialized implementation contract can be taken over by an attacker,
-    # which may impact the proxy. Manually invoking initializer on implementation contract
-    estate.initialize(
-        ESTATE_NAME,
-        ESTATE_SYMBOL,
-        ESTATE_BASE_URI_TEMPLATE.format(environment=environment),
-        land_address,
-        {"from": account, "gas_limit": 1000000},
-    )
+    return estate
 
 
-def initialize_state(estate_address: str, land_address: str, environment="dev"):
-    account = get_account()
+def initialize_estate(
+    estate_address: str,
+    land_address: str,
+    environment="dev",
+    account: Optional[Account] = None,
+):
+    if not account:
+        account = get_account()
     estate = Contract.from_abi("HighriseEstate", estate_address, HighriseEstate.abi)
     estate.initialize(
         ESTATE_NAME,
@@ -40,8 +39,10 @@ def deploy_proxy(
     land_address: str,
     proxy_admin_address: str,
     environment="dev",
-):
-    account = get_account()
+    account: Optional[Account] = None,
+) -> Contract:
+    if not account:
+        account = get_account()
     estate = Contract.from_abi(
         "HighriseEstate", estate_impl_address, HighriseEstate.abi
     )
@@ -52,16 +53,20 @@ def deploy_proxy(
         ESTATE_BASE_URI_TEMPLATE.format(environment=environment),
         land_address,
     )
-    TransparentUpgradeableProxy.deploy(
+    proxy = TransparentUpgradeableProxy.deploy(
         estate.address,
         proxy_admin_address,
         estate_encoded_initializer_function,
         {"from": account, "gas_limit": 1000000},
     )
+    return proxy
 
 
-def grant_roles(land_address: str, estate_address: str):
-    account = get_account()
+def grant_roles(
+    land_address: str, estate_address: str, account: Optional[Account] = None
+):
+    if not account:
+        account = get_account()
     land_proxy_with_abi = Contract.from_abi(
         "HighriseLand", land_address, HighriseLand.abi
     )
@@ -70,3 +75,34 @@ def grant_roles(land_address: str, estate_address: str):
         estate_address,
         {"from": account},
     ).wait(1)
+
+
+def verify_estate(estate_address: str):
+    contract = HighriseEstate.at(estate_address)
+    HighriseEstate.publish_source(contract)
+
+
+def verify_estate_proxy(proxy_address: str):
+    contract = TransparentUpgradeableProxy.at(proxy_address)
+    TransparentUpgradeableProxy.publish_source(contract)
+
+
+def deploy_estate(
+    proxy_admin: Contract,
+    land_address: str,
+    environment: str = "dev",
+    account: Optional[Account] = None,
+) -> tuple[Contract, Contract]:
+    if not account:
+        account = get_account()
+    estate = deploy_estate_implementation(account)
+    # Do not leave an implementation contract uninitialized. An uninitialized implementation contract can be taken over by an attacker,
+    # which may impact the proxy. Manually invoking initializer on implementation contract
+    initialize_estate(estate.address, land_address, environment, account)
+    # Deploy estate proxy
+    estate_proxy = deploy_proxy(
+        estate.address, land_address, proxy_admin.address, environment, account
+    )
+    # Grant roles
+    grant_roles(land_address, estate_proxy.address, account)
+    return estate_proxy, estate
--- scripts/land.py
+++ scripts/land.py
@@ -1,11 +1,27 @@
-from brownie import Contract, HighriseLand, TransparentUpgradeableProxy, config, network
+from typing import Optional
+
+from brownie import Contract, HighriseLand, TransparentUpgradeableProxy, network
+from eth_account import Account
 
 from . import LAND_BASE_URI_TEMPLATE, LAND_NAME, LAND_SYMBOL
 from .common import encode_function_data, get_account
 
 
-def initialize(land_address: str, environment: str = "dev"):
-    account = get_account()
+def deploy_land_implementation(account: Optional[Account] = None) -> Contract:
+    if not account:
+        account = get_account()
+    # Land
+    land = HighriseLand.deploy(
+        {"from": account},
+    )
+    return land
+
+
+def initialize(
+    land_address: str, environment: str = "dev", account: Optional[Account] = None
+):
+    if not account:
+        account = get_account()
     land = Contract.from_abi("HighriseLand", land_address, HighriseLand.abi)
     land.initialize(
         LAND_NAME,
@@ -15,8 +31,14 @@ def initialize(land_address: str, environment: str = "dev"):
     ).wait(1)
 
 
-def deploy_proxy(land_impl_address: str, proxy_admin_address: str, environment="dev"):
-    account = get_account()
+def deploy_proxy(
+    land_impl_address: str,
+    proxy_admin_address: str,
+    environment="dev",
+    account: Optional[Account] = None,
+) -> Contract:
+    if not account:
+        account = get_account()
     land = Contract.from_abi("HighriseLand", land_impl_address, HighriseLand.abi)
     land_encoded_initializer_function = encode_function_data(
         land.initialize,
@@ -24,15 +46,35 @@ def deploy_proxy(land_impl_address: str, proxy_admin_address: str, environment="
         LAND_SYMBOL,
         LAND_BASE_URI_TEMPLATE.format(environment=environment),
     )
-    TransparentUpgradeableProxy.deploy(
+    land_proxy = TransparentUpgradeableProxy.deploy(
         land_impl_address,
         proxy_admin_address,
         land_encoded_initializer_function,
         {"from": account, "gas_limit": 1000000},
-        publish_source=config["networks"][network.show_active()].get("verify"),
     )
+    return land_proxy
 
 
 def verify_proxy(proxy_address: str):
     contract = TransparentUpgradeableProxy.at(proxy_address)
     TransparentUpgradeableProxy.publish_source(contract)
+
+
+def verify_land(land_address: str):
+    contract = HighriseLand.at(land_address)
+    HighriseLand.publish_source(contract)
+
+
+def deploy_land(
+    proxy_admin: Contract, environment: str = "dev", account: Optional[Account] = None
+) -> tuple[Contract, Contract]:
+    if not account:
+        account = get_account()
+    # Land
+    land = deploy_land_implementation(account)
+    # Do not leave an implementation contract uninitialized. An uninitialized implementation contract can be taken over by an attacker,
+    # which may impact the proxy. Manually invoking initializer on implementation contract
+    initialize(land.address, environment, account)
+    # Deploy land proxy
+    land_proxy = deploy_proxy(land.address, proxy_admin.address, environment, account)
+    return land_proxy, land
