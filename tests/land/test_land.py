6dc0db1f1c17a32aadc39fb7f9984f32b1de452e
Marko Culinovic <marko.culinovic@gmail.com>
2022-03-14 10:41:50 +0100

    Opensea proxy test

--- tests/land/test_land.py
+++ tests/land/test_land.py
@@ -2,11 +2,14 @@ from random import randint
 
 import pytest
 from brownie import exceptions
+from brownie.network.contract import ProjectContract
+from brownie.network.account import LocalAccount
+from brownie.network import accounts
 
 from .. import LAND_BASE_TOKEN_URI, LAND_NAME, LAND_SYMBOL
 
 
-def test_initialization(land_contract, admin):
+def test_initialization(land_contract: ProjectContract, admin: LocalAccount):
     assert land_contract.name() == LAND_NAME
     assert land_contract.symbol() == LAND_SYMBOL
     assert land_contract.hasRole(land_contract.DEFAULT_ADMIN_ROLE.call(), admin)
@@ -16,7 +19,7 @@ def test_initialization(land_contract, admin):
     assert royalty_info[1] == 5  # Percentage is set to 5
 
 
-def test_mint(land_contract, admin, alice):
+def test_mint(land_contract: ProjectContract, admin: LocalAccount, alice: LocalAccount):
     token_id = 4324239
     tx = land_contract.mint(alice, token_id, {"from": admin})
     tx.wait(1)
@@ -29,14 +32,21 @@ def test_mint(land_contract, admin, alice):
     assert tx.events[0]["tokenId"] == str(token_id)
 
 
-def test_mint_without_role(land_contract, alice, charlie):
+def test_mint_without_role(
+    land_contract: ProjectContract, alice: LocalAccount, charlie: LocalAccount
+):
     token_id = 3213145
     with pytest.raises(exceptions.VirtualMachineError) as excinfo:
         land_contract.mint(charlie, token_id, {"from": alice})
     assert "revert: AccessControl" in str(excinfo.value)
 
 
-def test_bind_to_estate(land_contract, admin, alice, charlie):
+def test_bind_to_estate(
+    land_contract: ProjectContract,
+    admin: LocalAccount,
+    alice: LocalAccount,
+    charlie: LocalAccount,
+):
     token_ids = [randint(1, 100000) for _ in range(2)]
     # 1. Mint the tokens
     for token_id in token_ids:
@@ -55,7 +65,9 @@ def test_bind_to_estate(land_contract, admin, alice, charlie):
     assert land_contract.ownerTokens(alice) == []
 
 
-def test_change_uri(land_contract, admin, alice):
+def test_change_uri(
+    land_contract: ProjectContract, admin: LocalAccount, alice: LocalAccount
+):
     # First mint token so that we can fetch tokenURI
     token_id = 4324239
     land_contract.mint(alice, token_id, {"from": admin}).wait(1)
@@ -71,3 +83,26 @@ def test_change_uri(land_contract, admin, alice):
     with pytest.raises(exceptions.VirtualMachineError) as excinfo:
         land_contract.setBaseTokenURI(FAIL_URI, {"from": alice}).wait(1)
     assert "revert: AccessControl" in str(excinfo.value)
+
+
+def test_opensea_proxy(
+    land_contract: ProjectContract,
+    opensea_proxy_registry: ProjectContract,
+    admin: LocalAccount,
+    alice: LocalAccount,
+    charlie: LocalAccount,
+):
+    # Set opensea proxy account for alice
+    alice_opensea_proxy = accounts.add()
+    opensea_proxy_registry.setProxy(
+        alice.address, alice_opensea_proxy.address, {"from": admin}
+    )
+    # Mint to alice
+    token_id = 4324239
+    land_contract.mint(alice, token_id, {"from": admin}).wait(1)
+    # Transfer triggered
+    land_contract.safeTransferFrom(
+        alice, charlie, token_id, {"from": alice_opensea_proxy}
+    ).wait(1)
+    assert land_contract.ownerTokens(alice) == []
+    assert land_contract.ownerTokens(charlie) == [token_id]
