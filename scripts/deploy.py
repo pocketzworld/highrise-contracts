c27cf13b2a4db7b7a20cf62ac88cbd9ad56f74ed
Marko Culinovic <marko.culinovic@gmail.com>
2022-03-14 11:39:07 +0100

    Adjust scripts for opensea proxy registry

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -4,7 +4,7 @@ from brownie import config, network
 
 from .common import get_account
 from .estate import deploy_estate, verify_estate, verify_estate_proxy
-from .helpers import deploy_proxy_admin
+from .helpers import deploy_proxy_admin, opensea_proxy_registry_address
 from .land import deploy_land, verify_land, verify_proxy
 
 environment = os.environ["ENVIRONMENT_NAME"]
@@ -15,9 +15,10 @@ def deploy():
     print(f"Deploying for Highrise {environment} environment")
     print(f"Deploying from {account} to {network.show_active()}")
     proxy_admin = deploy_proxy_admin(account)
-    land_proxy, land = deploy_land(proxy_admin, environment, account)
+    opensea_address = opensea_proxy_registry_address(account)
+    land_proxy, land = deploy_land(proxy_admin, opensea_address, environment, account)
     estate_proxy, estate = deploy_estate(
-        proxy_admin, land_proxy.address, environment, account
+        proxy_admin, land_proxy.address, opensea_address, environment, account
     )
 
     if config["networks"][network.show_active()].get("verify"):
