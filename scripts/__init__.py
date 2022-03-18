34586d5dbbcd5b17e01a1c725bd1de8f7b086e4c
Marko Culinovic <marko.culinovic@gmail.com>
2022-03-18 09:32:20 +0100

    Change land base uri to point to cdn

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,8 +1,6 @@
 LAND_NAME = "TEST Highrise LAND"
 LAND_SYMBOL = "TESTLAND"
-LAND_BASE_URI_TEMPLATE = (
-    "https://highrise-land-{environment}.s3.amazonaws.com/metadata/"
-)
+LAND_BASE_URI_TEMPLATE = "https://cdn-land-{environment}.joinhighrise.com/metadata/"
 
 ESTATE_NAME = "TEST Highrise ESTATE"
 ESTATE_SYMBOL = "TESTESTATE"
