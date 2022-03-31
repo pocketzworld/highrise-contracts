67b28a6e7dc3331fb2b3af58b6bf6955d340edb5
Marko Culinovic <marko.culinovic@gmail.com>
2022-03-31 09:45:42 +0200

    Restructure imx upgrade script

--- scripts/upgrade/imx.py
+++ scripts/upgrade/imx.py
@@ -27,18 +27,21 @@ def verify_land_imx(imx_land_address: str):
     HighriseLandImx.publish_source(contract)
 
 
-def upgrade_imx(proxy_admin_address: str, land_proxy_address: str):
-    imx_core_address = config["networks"][network.show_active()].get("imxCoreAddress")
+def upgrade_implementation(
+    proxy_admin_address: str,
+    land_proxy_address: str,
+    imx_land_address: str,
+    account: Optional[Account] = None,
+):
+    imx_core_address = config["networks"][network.show_active()].get("imxCore")
     if not imx_core_address:
         print(
             f"IMX not supported on network: {config['networks'][network.show_active()]}"
         )
-        return
-    account = get_account()
-    # Deploy land imx contract, contract is initialized with constructor
-    imx_land = deploy_land_imx(account)
-    # When deploying to real network there seems to be a lag with infura connection before contract is available
-    sleep(2)
+        imx_core_address = "0xdEbC221d934D26F9CB96D2F7FfA0Abd12A997948"  # Mock address for other networks
+    print(f"Using IMX address: {imx_core_address}")
+    if not account:
+        account = get_account()
     # Fetch proxy admin and land_proxy
     proxy_admin = Contract.from_abi("ProxyAdmin", proxy_admin_address, ProxyAdmin.abi)
     land_proxy = Contract.from_abi(
@@ -46,16 +49,38 @@ def upgrade_imx(proxy_admin_address: str, land_proxy_address: str):
         land_proxy_address,
         TransparentUpgradeableProxy.abi,
     )
+    imx_land = Contract.from_abi(
+        "HighriseLandImx", imx_land_address, HighriseLandImx.abi
+    )
     # Do the upgrade
     upgrade_transaction = upgrade(
         account,
         land_proxy,
         imx_land.address,
         proxy_admin,
-        imx_land.initImx,  # This is not real initializer but we still need to init imx address
+        imx_land.initIMX,  # This is not real initializer but we still need to init imx address
         imx_core_address,
     )
     upgrade_transaction.wait(1)
+
+
+def upgrade_imx(proxy_admin_address: str, land_proxy_address: str):
+    imx_core_address = config["networks"][network.show_active()].get("imxCore")
+    if not imx_core_address:
+        print(
+            f"IMX not supported on network: {config['networks'][network.show_active()]}"
+        )
+        imx_core_address = "0xdEbC221d934D26F9CB96D2F7FfA0Abd12A997948"  # Mock address for other networks
+    print(f"Using IMX address: {imx_core_address}")
+    account = get_account()
+    # Deploy land imx contract, contract is initialized with constructor
+    imx_land = deploy_land_imx(account)
+    # When deploying to real network there seems to be a lag with infura connection before contract is available
+    sleep(2)
+    # Upgrade implementation
+    upgrade_implementation(
+        proxy_admin_address, land_proxy_address, imx_land.address, account
+    )
 
     # Publish and verify implementation contract
     if config["networks"][network.show_active()].get("verify"):
