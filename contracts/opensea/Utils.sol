6f80641c8a78683cd57745f5e7097a6dd54cb931
Marko Culinovic <marko.culinovic@gmail.com>
2022-04-14 10:57:34 +0200

    Set solc version to =0.8.12

--- contracts/opensea/Utils.sol
+++ contracts/opensea/Utils.sol
@@ -1,13 +1,15 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.12;
+pragma solidity =0.8.12;
 
 /*
-* This is 1-on-1 mapping from opensea example 
-* https://github.com/ProjectOpenSea/opensea-creatures/blob/master/contracts/ERC721Tradable.sol
-* Retaining contract structure for readability
-*/
+ * This is 1-on-1 mapping from opensea example
+ * https://github.com/ProjectOpenSea/opensea-creatures/blob/master/contracts/ERC721Tradable.sol
+ * Retaining contract structure for readability
+ */
 
-contract OwnableDelegateProxy {}
+contract OwnableDelegateProxy {
+
+}
 
 /**
  * Used to delegate ownership of a contract to another address, to save on unneeded transactions to approve contract use for users
--- interfaces/IHighriseLand.sol
+++ interfaces/IHighriseLand.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.4;
+pragma solidity =0.8.12;
 
 interface IHighriseLand {
     function mint(address user, uint256 tokenId) external;
