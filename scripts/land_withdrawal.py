78e9e49b028b81218675ca2f052d29de2f4096b4
Marko Culinovic <marko.culinovic@gmail.com>
2022-05-12 15:17:26 +0200

    Add withdrawal contract script

---
+++ scripts/land_withdrawal.py
@@ -0,0 +1,62 @@
+from brownie import (
+    Contract,
+    HighriseLand,
+    HighriseLandWithdrawal,
+    accounts,
+    config,
+    network,
+)
+
+from .common import FORKED_LOCAL_ENVIRONMENTS, LOCAL_BLOCKCHAIN_ENVIRONMENTS
+
+ACCOUNT_NAME = "one"
+ADMIN_ACCOUNT = "dev-account"
+
+
+def deploy_land_withdrawal(land_address: str):
+    """Land withdrawal must be deployed after land contract is deployed"""
+    account = accounts.load(ACCOUNT_NAME)
+    withdrawal_contract = HighriseLandWithdrawal.deploy(
+        land_address,
+        {"from": account},
+        publish_source=config["networks"][network.show_active()].get("verify"),
+    )
+    grant_roles(withdrawal_contract.address, land_address)
+
+
+def grant_roles(withdrawal_contract_address: str, land_address: str):
+    account = accounts.load(ADMIN_ACCOUNT)
+    # Grant roles
+    land_proxy = Contract.from_abi("HighriseLand", land_address, HighriseLand.abi)
+    land_proxy.grantRole(
+        land_proxy.MINTER_ROLE(),
+        withdrawal_contract_address,
+        {"from": account},
+    ).wait(1)
+
+
+def disable(withdrawal_contract_address: str):
+    account = accounts.load(ACCOUNT_NAME)
+    withdrawal_contract = Contract.from_abi(
+        "HighriseLandWithdrawal",
+        withdrawal_contract_address,
+        HighriseLandWithdrawal.abi,
+    )
+    withdrawal_contract.disable({"from": account}).wait(1)
+
+
+def enable(withdrawal_contract_address: str):
+    if (
+        network.show_active()
+        in LOCAL_BLOCKCHAIN_ENVIRONMENTS + FORKED_LOCAL_ENVIRONMENTS
+    ):
+        print("Error: Script can only be run on real network")
+        return
+
+    withdrawal_contract = Contract.from_abi(
+        "HighriseLandWithdrawal",
+        withdrawal_contract_address,
+        HighriseLandWithdrawal.abi,
+    )
+    account = accounts.load(ACCOUNT_NAME)
+    withdrawal_contract.enable({"from": account}).wait(1)
